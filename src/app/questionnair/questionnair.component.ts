99de52282722e080801f4a262e98d622583b251c
marcialwushu <marcialwushu@gmail.com>
2019-08-01 17:13:59 -0300

    questionnairComponent listing fixed questions

--- src/app/questionnair/questionnair.component.ts
+++ src/app/questionnair/questionnair.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Question } from '../models/question.model';
+import { QuestionnairService } from '../shared/questionnair.service';
 
 @Component({
   selector: 'app-questionnair',
@@ -7,9 +9,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuestionnairComponent implements OnInit {
 
-  constructor() { }
+  nextQuestion: Question;
+  isOnLastQuestion = false;
+
+  constructor(private questionnairService: QuestionnairService) { }
 
   ngOnInit() {
+    this.nextQuestion = this.questionnairService.getNextQuestion();
+  }
+
+  next() {
+    this.isOnLastQuestion = this.questionnairService.isLastQuestion();
+    this.nextQuestion = this.questionnairService.getNextQuestion();
   }
 
 }
